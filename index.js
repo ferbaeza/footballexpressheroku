867f881d8deb1d9400fad3bc561e455224b191de
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-02-07 23:58:11 +0100

    uodate

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose'); //Mongodb connector
 const methodOverride = require('method-override')
 //const port = 3000;
-const port= process.env.PORT || 5000; // heroku
+const port= process.env.PORT || 3000; // heroku
 
 const Player = require('./models/player');
 
